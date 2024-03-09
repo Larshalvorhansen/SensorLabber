8ba80a36c08eff827605038a5ab4757ba30565d5
lhh <larshalvorhansen1@gmail.com>
2024-03-09 14:38:06 +0100

    legg til datablad

---
+++ radarLab/python/plotIQ.py
@@ -0,0 +1,29 @@
+import math
+# import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+x = [] 
+y = []
+
+with open('radarLab/data/BigBrainHighIQdata.csv','r') as csvfile: 
+    plots = csv.reader(csvfile, delimiter = ',')
+
+for row in plots: 
+        x.append(row[0]) 
+        y.append(int(row[2])) 
+
+vec = []
+vec2 = []
+
+for i in range(0,100):
+    print(i)
+    vec.append(i)
+    vec2.append(100-i)
+
+
+print(vec)
+print(vec2)
+
+plt.plot(vec,vec2)
+plt.show()
