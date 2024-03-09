852d1de6ffb9c3f175460e4b7d1c2a985502f065
PaulusKlaus <122972254+PaulusKlaus@users.noreply.github.com>
2024-03-09 15:52:05 +0100

    ploting av filtre

--- radarLab/python/Filterplotter.py
+++ radarLab/python/Filterplotter.py
@@ -15,7 +15,7 @@ plt.rcParams['lines.linewidth'] = 2.5
 #Eksportere data
 header = []
 data = []
-filename = '/Users/polinaermakova/Documents/GitHub/SensorLabber/radarLab/data/filter1.csv'
+filename = 'data/filter1(2).csv'
 #Henter data fra csvfil
 with open(filename) as csvfile:
     csvreader = csv.reader(csvfile)
@@ -34,8 +34,8 @@ data5 = [(p[2]) for p in data]
 
 fig, (ax) = plt.subplots(1,1, figsize=(22,8))
 #Plotsemilogx
-ax.plot(time1,data4, label = 'Inngansignal s(t) ')
-ax.plot(time1,data5, label = 'Utgangsignal y(t) ')
+ax.semilogx(time1,data4, label = 'Inngansignal s(t) ')
+ax.semilogx(time1,data5, label = 'Utgangsignal y(t) ')
 #ax.plot(time,ch2, label = 'Utgangsignal y(t)')
 #Tittel og aksenavn
 #ax.set_title('Oscilloskop')
@@ -49,14 +49,70 @@ ax.set_ylabel('Dempling [dB]',fontsize=22)
 
 
 
-ax.axhline(y=-3, color='r', linestyle='--', label ='Dempning -3dB',)
+ax.axhline(y=17, color='r', linestyle='--', label ='Dempning 17dB',)
 #ax.axhline(x=29, color='g', linestyle='--', label ='Knekkfrekvens')
-ax.axvline(x=43, color='g', linestyle=':', label ='Knekfrekvense f$_c$ = 43 Hz')
+ax.axvline(x=3.5, color='g', linestyle=':', label ='Knekfrekvense f$_c$ = 3.5 Hz')
+ax.axvline(x=4800, color='g', linestyle=':', label ='Knekfrekvense f$_c$ = 4800 Hz')
 #Legend, loc = plassering
 ax.legend(loc='upper right') #Rutenett
 ax.grid(True)
-ax.set_ylim(-30,10)
-#ax.set_xlim(0, 50)
+ax.set_ylim(-10,30)
+ax.set_xlim(0.5, 50000)
+ax.legend(loc='best',fontsize=20) #Rutenett
+ax.grid(True)
+plt.show()
+
+#fig.savefig('signalrespons', dpi=300, bbox_inches='tight')
+
+
+
+
+
+header = []
+data = []
+filename = 'data/filter2.csv'
+#Henter data fra csvfil
+with open(filename) as csvfile:
+    csvreader = csv.reader(csvfile)
+    #Leser første linje i csv-fila (den med navn til kanalene)
+    header = next(csvreader)
+    for datapoint in csvreader:
+        values = [float(value) for value in datapoint]
+        data.append(values)
+#Legger inn data fra hver kanal i hver sin liste
+time1 = [(p[0]) for p in data]
+data4=[(p[1]) for p in data] #inngangsignalet 
+data5 = [(p[2]) for p in data]
+
+
+
+
+fig, (ax) = plt.subplots(1,1, figsize=(22,8))
+#Plotsemilogx
+ax.semilogx(time1,data4, label = 'Inngansignal s(t) ')
+ax.semilogx(time1,data5, label = 'Utgangsignal y(t) ')
+#ax.plot(time,ch2, label = 'Utgangsignal y(t)')
+#Tittel og aksenavn
+#ax.set_title('Oscilloskop')
+ax.set_xlabel('Frekvens Hz',fontsize=22)
+ax.set_ylabel('Demping (dB))',fontsize=22)
+#Legend, loc = plassering
+ax.legend(loc='upper right') #Rutenett
+ax.grid(True)
+ax.set_xlabel('Frekvens[Hz]',fontsize=22)
+ax.set_ylabel('Dempling [dB]',fontsize=22)
+
+
+
+ax.axhline(y=17, color='r', linestyle='--', label ='Dempning 17dB',)
+#ax.axhline(x=29, color='g', linestyle='--', label ='Knekkfrekvens')
+ax.axvline(x=3.5, color='g', linestyle=':', label ='Knekfrekvense f$_c$ = 3.5 Hz')
+ax.axvline(x=4800, color='g', linestyle=':', label ='Knekfrekvense f$_c$ = 4800 Hz')
+#Legend, loc = plassering
+ax.legend(loc='upper right') #Rutenett
+ax.grid(True)
+ax.set_ylim(-10,30)
+ax.set_xlim(0.5, 50000)
 ax.legend(loc='best',fontsize=20) #Rutenett
 ax.grid(True)
 plt.show()
